5d769f5f61d6ababb4e2b48458af76d63362aa32
NWSteenberg <nikolajws@protonmail.com>
2024-08-24 19:32:32 +0200

    Moved configurations into seperate dir Moved devcontainers into seperate dir Dynamically replace TERM in configurations

--- run-dev.sh
+++ run-dev.sh
@@ -4,13 +4,13 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 language=${1}
 if [ -z "${language}" ]; then
     echo "No language specified. Pick one that matches the following devcontainer spec:"
-    ls -a ${SCRIPTPATH} | grep "devcontainer-*.json"
+    ls -a ${SCRIPTPATH}/devcontainers | grep "devcontainer-*.json"
     exit 1
 fi
 
 devcontainer_name=".devcontainer-${language}.json"
 
-cp "${SCRIPTPATH}/${devcontainer_name}" .
+cp -f "${SCRIPTPATH}/devcontainers/${devcontainer_name}" .
 
 # Project is always named after the the parent directory
 project_name="$(basename $PWD)"
