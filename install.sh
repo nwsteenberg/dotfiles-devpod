ccaa9fac78ee1a9a408141cd50d9faec72558aec
NWSteenberg <nikolajws@protonmail.com>
2024-08-27 22:11:36 +0200

    Further detatched nvim. Allow install for env without brew

--- install.sh
+++ install.sh
@@ -1,24 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+function installBrewPackages() {
+    # Brew packages to install
+    brew_packages=(
+        npm
+        docker
+        tmux
+    )
 
-# Brew packages to install
-packages=(
-    npm
-    docker
-    tmux
-)
+    for brew_package in "${packages[@]}"; do
+        echo "Installing $package..."
+        /home/linuxbrew/.linuxbrew/bin/brew install "$package"
+    done
+}
 
-for package in "${packages[@]}"; do
-	echo "Installing $package..."
-	/home/linuxbrew/.linuxbrew/bin/brew install "$package"
-done
+# Brew
+if [[ -f $(which brew) ]]; then
+    installBrewPackages
+fi
 
-# Grab other resources from git
-git clone https://github.com/nwsteenberg/nvim.git "$PWD/dotfiles/.config/nvim"
+# Nvim
+if [[ -f $(which nvim) ]]; then
+    nvim_folder="$PWD/dotfiles/.config/nvim"
+    if [[ ! -d $nvim_folder ]]; then 
+        git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
+    else
+        pushd $nvim_folder 
+        git pull
+        popd
+    fi
+fi
 
-# install oh-my-bash
+# oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # Stow dotfiles
-stow --dir "$PWD/dotfiles" --target $HOME .
+stow --restow --dir "$PWD/dotfiles" --target $HOME .
+stow --restow --dir "$PWD/dotfiles/.config" --target $HOME/.config/ .
 
 echo "All packages from the setup script have been installed."
+
