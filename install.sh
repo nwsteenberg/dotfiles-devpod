ee3656e378a61e8975383830a333b6bcfc44415b
NWSteenberg <nikolajws@protonmail.com>
2024-08-28 21:18:16 +0200

    Removed brew

--- install.sh
+++ install.sh
@@ -1,32 +1,8 @@
 #!/usr/bin/env bash
-function installBrewPackages() {
-    # Brew packages to install
-    brew_packages=(
-        npm
-        docker
-        tmux
-    )
-
-    for brew_package in "${packages[@]}"; do
-        echo "Installing $package..."
-        /home/linuxbrew/.linuxbrew/bin/brew install "$package"
-    done
-}
-
-# Brew
-if [[ -f $(which brew) ]]; then
-    installBrewPackages
-fi
 
 # Nvim
 nvim_folder="$PWD/dotfiles/.config/nvim"
-if [[ ! -d $nvim_folder ]]; then 
-    git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
-else
-    pushd $nvim_folder 
-    git pull
-    popd
-fi
+git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
 
 # oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
