a9329c48d0dd6fa3a6b8d7a6a3bf21516f54db0e
NWSteenberg <nikolajws@protonmail.com>
2024-08-28 21:08:57 +0200

    Removed stow setup again, as this created unnecessary complexity

--- install.sh
+++ install.sh
@@ -19,22 +19,21 @@ if [[ -f $(which brew) ]]; then
 fi
 
 # Nvim
-if [[ -f $(which nvim) ]]; then
-    nvim_folder="$PWD/dotfiles/.config/nvim"
-    if [[ ! -d $nvim_folder ]]; then 
-        git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
-    else
-        pushd $nvim_folder 
-        git pull
-        popd
-    fi
+nvim_folder="$PWD/dotfiles/.config/nvim"
+if [[ ! -d $nvim_folder ]]; then 
+    git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
+else
+    pushd $nvim_folder 
+    git pull
+    popd
 fi
 
 # oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
-# Stow dotfiles
-stow --restow --dir "$PWD/dotfiles" --target $HOME .
-stow --restow --dir "$PWD/dotfiles/.config" --target $HOME/.config/ .
+# synlinks
+ln -sf "$PWD/dotfiles/.config/nvim" "$HOME/.config/nvim"
+ln -sf "$PWD/dotfiles/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$PWD/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "All packages from the setup script have been installed."
