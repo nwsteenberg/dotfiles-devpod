75f08b5ff1c7d9b680ca747e531df6d7d7b1d31f
nwsteenberg <nikolajws@protonmail.com>
2024-12-27 22:10:08 +0100

    Tryout zsh

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ nvim_folder="$PWD/dotfiles/.config/nvim"
 mkdir -p "$PWD/dotfiles/.config"
 git clone https://github.com/nwsteenberg/nvim.git "$nvim_folder"
 
-# oh-my-bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
 # symlinks
 mkdir -p "$HOME/.config"
 ln -sf "$PWD/dotfiles/.config/nvim" "$HOME/.config/nvim"
-ln -sf "$PWD/dotfiles/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$PWD/dotfiles/.zprofile" "$HOME/.zprofile"
 ln -sf "$PWD/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "All packages from the setup script have been installed."
